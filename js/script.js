62c241f1f5f09c814d8ed8a2b915849281b7c15a
Zachary Surgett <zsurgett@gmail.com>
2016-01-24 14:36:18 -0700

    Moved inline JS to my script.js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-/*globals $:true, jquery:true*/
+//IIFE to wrap the entire script
+(function($, window, document) {
+//The $ is now locally scoped
+
+
+/*************************
+*****FUNCTION CALLS*******
+*************************/
+$(function() {
+	//The DOM is ready
+	randomBackground();
+	scrollToSection();
+});
+
 
 /*************************
 ******JQUERY SECTION******
 *************************/
 
-$(document).on("load", randomBackground());
+
+
+/*************************
+********FUNCTIONS*********
+*************************/
 
 function randomBackground() {
 	"use strict";
@@ -17,7 +34,7 @@ function randomBackground() {
 
 	header = $('.header');
 	//create an array that will hold the names of the backgrounds to choose from
-	backgrounds = ['rainy-floor','seattle','space-jupiter', 'office', 'water', 'laptop-notepad'];
+	backgrounds = ['rainy-floor','seattle','space-jupiter', 'office', 'laptop-notepad'];
 
 	//set min to 1 and max to the length of the backgrounds array
 	min = 0;
@@ -33,3 +50,22 @@ function randomBackground() {
 	header.css('background-attachment', 'scroll');
 	header.css('background-size', 'cover');
 }
+
+// Scrolls to the selected menu item on the page
+function scrollToSection() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+					}, 1000);
+				return false;
+			}
+		}
+	});
+}
+
+}(window.jQuery, window, document));
