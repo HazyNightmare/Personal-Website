eb0a7833ac3c3cf2454d4c848b97efff55e116b5
Zachary Surgett <zsurgett@gmail.com>
2016-01-26 18:56:16 -0700

    Added a folder to hold all of the blog files

---
+++ blog/js/script.js
@@ -0,0 +1,67 @@
+//IIFE to wrap the entire script and keep all vars local
+(function($, window, document) {
+	"use strict";
+
+	/*************************
+	 *****FUNCTION CALLS*******
+	 *************************/
+	randomBackground();
+	scrollToSection();
+
+
+	/*************************
+	 ******JQUERY SECTION******
+	 *************************/
+
+
+
+	/*************************
+	 ********FUNCTIONS*********
+	 *************************/
+
+	function randomBackground() {
+		//Variable declarations
+		var header = null,
+			backgrounds = [],
+			min = null,
+			max = null,
+			chosen = null;
+
+		header = $('.header');
+		//create an array that will hold the names of the backgrounds to choose from
+		backgrounds = ['rainy-floor', 'seattle', 'space-jupiter', 'office', 'laptop-notepad'];
+
+		//set min to 1 and max to the length of the backgrounds array
+		min = 0;
+		max = backgrounds.length;
+
+		//Random number is generated
+		chosen = Math.floor(Math.random() * (max - min)) + min;
+
+		//Background of the .header class is changed depending on the result of the var chosen
+		header.css('background', 'url(imgs/' + backgrounds[chosen] + '.jpg)');
+		header.css('background-repeat', 'no-repeat');
+		header.css('background-position', 'center');
+		header.css('background-attachment', 'scroll');
+		header.css('background-size', 'cover');
+	}
+
+	// Scrolls to the selected menu item on the page
+	function scrollToSection() {
+		var target = null;
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+				target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	}
+
+}(window.jQuery, window, document));
